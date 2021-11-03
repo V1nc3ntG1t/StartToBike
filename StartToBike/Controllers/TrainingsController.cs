1513ba2415030451599f7ebb2581b35041472098
Dax Hamaekers <1875698hamaekers@zuyd.nl>
2021-11-03 20:31:46 +0100

    Added Training class

---
+++ StartToBike/Controllers/TrainingsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StartToBike.Models;
+
+namespace StartToBike.Controllers
+{
+    public class TrainingsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Trainings
+        public ActionResult Index()
+        {
+            return View(db.Trainings.ToList());
+        }
+
+        // GET: Trainings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Training training = db.Trainings.Find(id);
+            if (training == null)
+            {
+                return HttpNotFound();
+            }
+            return View(training);
+        }
+
+        // GET: Trainings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trainings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TrainingID,Title,Level")] Training training)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trainings.Add(training);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(training);
+        }
+
+        // GET: Trainings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Training training = db.Trainings.Find(id);
+            if (training == null)
+            {
+                return HttpNotFound();
+            }
+            return View(training);
+        }
+
+        // POST: Trainings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TrainingID,Title,Level")] Training training)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(training).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(training);
+        }
+
+        // GET: Trainings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Training training = db.Trainings.Find(id);
+            if (training == null)
+            {
+                return HttpNotFound();
+            }
+            return View(training);
+        }
+
+        // POST: Trainings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Training training = db.Trainings.Find(id);
+            db.Trainings.Remove(training);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
